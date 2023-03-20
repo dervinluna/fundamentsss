bded440192926dc6a485ef4c3234846e11e1b94a
dervinluna <dhernandezl24@miumg.edu.gt>
2023-03-19 20:45:38 -0600

    Initial commit

---
+++ src/main/java/com/funda/springboot/fundament/FundamentApplication.java
@@ -0,0 +1,13 @@
+package com.funda.springboot.fundament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funda/springboot/fundament/FundamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funda.springboot.fundament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
