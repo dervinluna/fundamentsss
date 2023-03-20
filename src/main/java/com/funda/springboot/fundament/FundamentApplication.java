c635556bee800394f008e7d481230554e54a348f
dervinluna <dhernandezl24@miumg.edu.gt>
2023-03-19 23:52:02 -0600

    first comen

--- src/main/java/com/funda/springboot/fundament/FundamentApplication.java
+++ src/main/java/com/funda/springboot/fundament/FundamentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FundamentApplication.class, args);
 	}
 
